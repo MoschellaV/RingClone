e883178aa80c557e050215b7158340effafd6d84
vinceMoschella <vincemoschella04@gmail.com>
2023-01-18 01:45:53 -0500

    implemented threading, notifies server when face is detected, bug encountered -- video feed freezes after sending a post request and program breaks

--- faceDetected.py
+++ faceDetected.py
@@ -1,20 +1,23 @@
 import cv2 as cv
+import requests
+import json
 
 def notify_server():
-    print("face detected")
     
-def draw_rect(frame, coordinates, detections):
+    url ="http://10.12.89.172:5051/post"
+    payload= { "status": "FACE DETECTED"}
+    headers = {"Content-Type": "application/json"}
+    
+    r = requests.post(url, json=payload, headers=headers)
+    
+    print("Face detected. OMG I just have to tell the server!")
+
+    
+def draw_rect(frame, coordinates, has_notified):
     '''
-    This fucntion draws a rectangle around detected faces and calls
-    the notify_server function once a single face is detected.
+    This fucntion draws a rectangle around detected faces.
     '''
-    # calling notify server so that a request can be send to the server
-    # then the client
-    if detections == 1:
-        notify_server()
-        
+    
     # drawing rectangle in frame
     for (x,y,w,h) in coordinates:
         cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
-
-    
--- main.py
+++ main.py
@@ -15,6 +15,8 @@ sleep(5)
 camera.stop_preview()
 '''
 import cv2 as cv
+from time import sleep
+from threading import Thread
 from faceDetected import notify_server, draw_rect
 
 capture = cv.VideoCapture(0) #to open Camera
@@ -22,7 +24,13 @@ capture = cv.VideoCapture(0) #to open Camera
 #accessing pretrained model
 pretrained_model = cv.CascadeClassifier("model.xml")
 
-face_frame_detections = 0
+has_notified = False
+
+# THERE IS A PROBLEM: if a face is detected we add one to the counter
+# however the counter is never reset so when a new person is detected
+# the notify_server function is not called
+
+t1 = Thread(target=notify_server)
 
 while True:
     boolean, frame = capture.read()
@@ -30,10 +38,24 @@ while True:
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         coordinate_list = pretrained_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3) 
             
-        face_frame_detections += 1
+        #face_frame_detections += 1
+        #print(face_frame_detections)
         
-        #draws rectangle around detected faces
-        draw_rect(frame, coordinate_list, face_frame_detections)
+        #Face coordinates detected (i.e. there is a face in view)
+        if len(coordinate_list) > 0:
+            
+            #notifying server if it has not already been notified
+            if not has_notified:
+                
+                t1.start()
+                #notify_server()
+                t1.join()
+                has_notified = True
+            
+            t2 = Thread(target=draw_rect, args=(frame, coordinate_list, has_notified))
+            t2.start()
+            #draw_rect(frame, coordinate_list, has_notified)
+            t2.join()
             
         # Display detected face
         cv.imshow("Live Face Detection", frame)
