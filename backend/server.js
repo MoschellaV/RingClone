d519c1bde47a887226e9348cbe49631ea8c45743
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2023-01-18 01:43:51 -0500

    added post request to server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 const PORT = 5051;
 const express = require("express");
-const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
 
+app.post("/post", (req, res) => {
+    let { status } = req.body;
+    console.log(status);
+});
+
+// server listening
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
