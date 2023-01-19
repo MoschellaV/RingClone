091a4f46d291cb4637cd23ffcd163f627ddf416d
vinceMoschella <vincemoschella04@gmail.com>
2023-01-18 21:58:14 -0500

    fixed bugs sends server message when face is detected gets the time of face detection and also sends it to the server

--- main.py
+++ main.py
@@ -1,69 +1,44 @@
-'''
-from picamera import PiCamera
-from time import sleep
-
-import cv2 as cv
-print(cv.__version__)
-
-
-camera = PiCamera()
-
-camera.resolution = (600,400)
-
-camera.start_preview()
-sleep(5)
-camera.stop_preview()
-'''
 import cv2 as cv
 from time import sleep
 from threading import Thread
-from faceDetected import notify_server, draw_rect
+from toServer import notify_server
+from detectionAndCamera import draw_rect
 
-capture = cv.VideoCapture(0) #to open Camera
+# to open Camera
+capture = cv.VideoCapture(0) 
 
-#accessing pretrained model
+# accessing pretrained model
 pretrained_model = cv.CascadeClassifier("model.xml")
 
-has_notified = False
-
-# THERE IS A PROBLEM: if a face is detected we add one to the counter
-# however the counter is never reset so when a new person is detected
-# the notify_server function is not called
-
-t1 = Thread(target=notify_server)
+counter = 0
 
 while True:
     boolean, frame = capture.read()
     if boolean == True:
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         coordinate_list = pretrained_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3) 
-            
-        #face_frame_detections += 1
-        #print(face_frame_detections)
         
-        #Face coordinates detected (i.e. there is a face in view)
+        # face coordinates detected (i.e. there is a face in view)
         if len(coordinate_list) > 0:
-            
-            #notifying server if it has not already been notified
-            if not has_notified:
+    
+            # implemented a counter so that the server is not spammed with requests
+            if counter > 100:
+                notify_server()
+                counter = 0
                 
-                t1.start()
-                #notify_server()
-                t1.join()
-                has_notified = True
-            
-            t2 = Thread(target=draw_rect, args=(frame, coordinate_list, has_notified))
-            t2.start()
-            #draw_rect(frame, coordinate_list, has_notified)
-            t2.join()
-            
-        # Display detected face
+            draw_rect(frame, coordinate_list)
+         
+        counter +=1
+        print(counter)
+
+        # display detected face
         cv.imshow("Live Face Detection", frame)
-       
+           
         # condition to break out of while loop
+        # clicking the 'x' key
         if cv.waitKey(20) == ord('x'):
             break
-        
+
 capture.release()
 cv.destroyAllWindows()
             
